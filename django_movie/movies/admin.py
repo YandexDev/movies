e5b482b317a96f8a1236f6f4799faf351536fad7
Andrei Pervyi <taxidevelop@yandex.ru>
2022-02-19 20:36:24 +0400

    modified admin_panel

--- django_movie/movies/admin.py
+++ django_movie/movies/admin.py
@@ -2,11 +2,81 @@ from django.contrib import admin
 
 from .models import Category, Actor, Genre, Movie, MovieShots, RatingStar, Rating, Reviews
 
-admin.site.register(Category)
-admin.site.register(Actor)
-admin.site.register(Genre)
-admin.site.register(Movie)
-admin.site.register(MovieShots)
-admin.site.register(RatingStar)
-admin.site.register(Rating)
-admin.site.register(Reviews)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'url')
+    list_display_links = ('name',)
+
+
+class ReviewInLine(admin.TabularInline):
+    model = Reviews
+    extra = 0
+    readonly_fields = ('name', 'email')
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url', 'draft')
+    list_filter = ('category', 'year')
+    search_fields = ('title', 'category__name')
+    inlines = [ReviewInLine]
+    save_on_top = True
+    save_as = True
+    list_editable = ('draft', )
+    fieldsets = (
+        (None, {
+            'fields': (('title', 'tagline'), )
+        }),
+        (None, {
+            'fields': ('description', 'poster')
+        }),
+        (None, {
+            'fields': (('year', 'world_premiere', 'country'),)
+        }),
+        (None, {
+            'fields': (('actors', 'directors', 'genres', 'category'),)
+        }),
+        ('Бюджет', {
+            'classes': ('collapse', ),
+            'fields': (('budget', 'fees_in_usa', 'fees_in_world'),)
+        }),
+        ('Опции', {
+            'fields': (('url', 'draft'),)
+        }),
+    )
+
+
+@admin.register(Reviews)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'parent', 'movie', 'id')
+    readonly_fields = ('name', 'email')
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'url')
+
+
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'age')
+
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ('movie', 'ip')
+
+
+@admin.register(MovieShots)
+class MovieShotsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'movie')
+
+# admin.site.register(Category, CategoryAdmin)
+# admin.site.register(Actor)
+# admin.site.register(Genre)
+# admin.site.register(Movie)
+# admin.site.register(MovieShots)
+# admin.site.register(RatingStar)
+# admin.site.register(Rating)
+# admin.site.register(Reviews)
--- django_movie/movies/views.py
+++ django_movie/movies/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
 from django.views import View
 from django.views.generic import ListView, DetailView
 
-from .models import Movie
 from .forms import ReviewForm
+from .models import Movie
+
 
 class MoviesView(ListView):
     """Список фильмов"""
@@ -29,6 +30,8 @@ class AddReview(View):
         movie = Movie.objects.get(id=pk)
         if form.is_valid():
             form = form.save(commit=False)
+            if request.POST.get('parent', None):
+                form.parent_id = int(request.POST.get('parent'))
             form.movie = movie
             form.save()
         return redirect(movie.get_absolute_url())
